30d67018c7175f68061574a20b8cd7cd76a67427
Santtu Lintervo <santervo@gmail.com>
2016-06-08 14:36:57 +0300

    Added react

--- app/index.js
+++ app/index.js
@@ -1,2 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
 require('./main.css')
-document.write("HELLO!!!")
+
+ReactDOM.render(
+  React.createElement('h1', null, 'Hello world'),
+  document.getElementById('app')
+);
