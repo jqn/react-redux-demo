1d105d07ad3062b342977908f50f039e5ec587da
Santtu Lintervo <santervo@gmail.com>
2016-09-06 22:16:07 +0300

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import router from './router'
-
+import provider from './provider'
 import bootstrapStyles from 'bootstrap-sass/assets/stylesheets/_bootstrap.scss'
 
 const appRoot = document.createElement('div')
 document.body.appendChild(appRoot)
 
-ReactDOM.render(router, appRoot)
+ReactDOM.render(provider, appRoot)
---
+++ src/provider.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
+import router from './router'
+
+const provider = <Provider store={store}>{router}</Provider>
+
+export default provider
