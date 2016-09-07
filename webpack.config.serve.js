fd00f00bd9ce42c981a11065aeea5e63875fd44a
Santtu Lintervo <santervo@gmail.com>
2016-09-07 12:45:27 +0300

    Updated react hot loader. Added object spread plugin to babel.

--- webpack.config.serve.js
+++ webpack.config.serve.js
@@ -17,7 +17,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['react-hot', 'babel']
+        loaders: ['babel']
       },
       {
         test: /\.scss$/,
